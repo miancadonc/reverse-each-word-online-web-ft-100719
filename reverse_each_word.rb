c315141e362ef079bb1c404c11732ddcdcf597be
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 00:06:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+#def reverse_each_word(phrase)
+ # new_phrase = []
+  #split_phrase = phrase.split(" ")
+  #split_phrase.each {|word| new_phrase << "#{word.reverse}"}
+  #new_phrase.join(" ")
+#end
+
+def reverse_each_word(phrase)
+  split_phrase = phrase.split(" ")
+  new_phrase = split_phrase.collect {|word| word.reverse}
+  new_phrase.join(" ")
+end
